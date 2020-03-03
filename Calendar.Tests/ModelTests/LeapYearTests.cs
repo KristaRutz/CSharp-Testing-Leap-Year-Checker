9fc019e94bfd2a856b8f6d7ac573b894f37cbfcc
Andriy Veremyeyev and Krista Rutz <example@example.com>
2020-03-02 16:58:50 -0800

    implement tests to ensure properly running logic for leap year

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,37 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.Tests
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    public void IsLeapYear_NumberDivisibleByFour_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2012));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_NumberNotDivisibleByFour_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1999));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfOneHundred_False()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(false, testLeapYear.IsLeapYear(1900));
+    }
+
+    [TestMethod]
+    public void IsLeapYear_MultiplesOfFourHundred_True()
+    {
+      LeapYear testLeapYear = new LeapYear();
+      Assert.AreEqual(true, testLeapYear.IsLeapYear(2000));
+    }
+  }
+}
